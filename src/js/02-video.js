852f5775170ea46866ff5b64f6e9c493bf0cb666
TZaidel <8tzaidel@gmail.com>
2023-11-21 09:42:09 +0100

    add vimeo and script for feedback form

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,18 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
+
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe)
+const CURRENT_TIME ='videoplayer-current-time'
+ 
+const savedTime = localStorage.getItem(CURRENT_TIME) || 0;
+console.log(savedTime)
+
+player.setCurrentTime(savedTime);
+
+const saveTime = function (data) {
+    localStorage.setItem(CURRENT_TIME, data.seconds);
+};
+
+player.on("timeupdate", throttle(saveTime, 1000));
 
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,52 @@
+import throttle from 'lodash.throttle'
+const form = document.querySelector('.feedback-form')
+const email = document.querySelector('input')
+const textarea = document.querySelector('.feedback-form textarea')
+const FEEDBACK_KEY = "feedback-form-state"
+
+form.addEventListener('submit', onSubmit)
+form.addEventListener('input', throttle(onInput, 500))
+
+const formData ={}
+
+getFromLocal(FEEDBACK_KEY)
+
+
+function onSubmit(event) {
+    event.preventDefault()
+    event.currentTarget.reset()
+    localStorage.removeItem(FEEDBACK_KEY)
+}
+
+
+function onInput(event) {
+    formData[event.target.name] = event.target.value
+    //*or
+//     const formData = {
+//     email: form.email.value,
+//     message: form.message.value,
+//   };
+    
+    const strMes = JSON.stringify(formData)
+
+    localStorage.setItem(FEEDBACK_KEY, strMes)
+
+    const savedMsg = localStorage.getItem(FEEDBACK_KEY)
+    const parsedMsg = JSON.parse(savedMsg)
+    console.log(parsedMsg)
+}
+
+
+function getFromLocal(key) {
+
+    const savedText = localStorage.getItem(key)
+    console.log(savedText)
+    const parsedText = JSON.parse(savedText)
+
+        if (savedText) {
+            form[0].value = parsedText.email
+            form[1].value = parsedText.message
+        }
+}
+
 
