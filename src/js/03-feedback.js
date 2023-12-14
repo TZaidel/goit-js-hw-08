14e038457e890b6623d342f1f544ae553b6dd116
TZaidel <8tzaidel@gmail.com>
2023-12-14 06:30:10 +0100

    edit formdata in task 3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,7 +1,5 @@
 import throttle from 'lodash.throttle'
 const form = document.querySelector('.feedback-form')
-const email = document.querySelector('input')
-const textarea = document.querySelector('.feedback-form textarea')
 const FEEDBACK_KEY = "feedback-form-state"
 
 form.addEventListener('submit', onSubmit)
@@ -16,15 +14,15 @@ getFromLocal(FEEDBACK_KEY)
 
 function onSubmit(event) {
     event.preventDefault()
-    // if (!formData.email.trim() || !formData.message.trim()) {
-    //     alert('Please fill all fields')
-    //     return
-    // }
+    if (!formData.email.trim() || !formData.message.trim()) {
+       return alert('Please fill all fields')
+    }
     console.log(formData)
-    event.currentTarget.reset()
+    form.reset()
+    formData.email = ""
+    formData.message = ""
     localStorage.removeItem(FEEDBACK_KEY)
 }
-
 
 function onInput(event) {
     formData[event.target.name] = event.target.value
@@ -37,15 +35,14 @@ function onInput(event) {
     localStorage.setItem(FEEDBACK_KEY, strMes)
 }
 
-
 function getFromLocal(key) {
     const savedText = localStorage.getItem(key)
     const parsedText = JSON.parse(savedText)
-
         if (savedText) {
             form[0].value = parsedText.email
             form[1].value = parsedText.message
-        }
+            formData.email = parsedText.email || ''
+            formData.message = parsedText.message || ''
+        }    
 }
-
 
